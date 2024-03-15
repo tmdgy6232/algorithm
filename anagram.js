e7d2115005d5f78931e96ec1b889cc8457c672ce
seunghyo <tmdgy6232@naver.com>
2024-03-15 23:12:40 +0900

    anagram 알고리즘 구현

---
+++ anagram.js
@@ -0,0 +1,28 @@
+/**
+ * anagram
+ */
+
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letter exists, incremenmt, otherwise set to 1
+    lookup[letter] ? (letter[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
